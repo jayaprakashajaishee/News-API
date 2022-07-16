0bb4cfa38c037e08783eaaae55d61a39cd7d406c
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2022-07-16 13:29:20 +0530

    sources added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import sources from "./sources.js";
+
 const app = express();
 
 //init request
 app.get("/", (req, res) => {
   res.send("server is ready");
+});
+
+//return products
+app.get("/api/sources", (req, res) => {
+  res.send(sources);
 });
 
 const port = process.env.port || 5003;
